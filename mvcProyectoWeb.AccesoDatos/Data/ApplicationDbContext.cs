142c135c1c8ec0270e7695ad1d6745d1b8363d5f
michael wilfredo peña luis <michaelwilfredopenaluis@gmail.com>
2024-04-14 19:58:02 -0400

    Agregar archivos de proyecto.

---
+++ mvcProyectoWeb.AccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using mvcProyectoWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvcProyectoWeb.AccesoDatos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+        // poner aqui todos los modelo que se crean
+        public DbSet<Almacen> Almacen { get; set; }
+    }
+}
